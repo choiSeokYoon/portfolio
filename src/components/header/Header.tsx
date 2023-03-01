e0769aedea5d06e722f679ba2ba133c879f27199
WKWKA <ctd0606@naver.com>
2023-03-01 19:37:20 +0900

    .

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import './Header.scss'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <div className='container'>
+            <div className='nav'>
+                <div className='logo'>
+                    <h2>SY's</h2>
+                </div>
+                <ul className='menu'>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/project">Project</Link></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
